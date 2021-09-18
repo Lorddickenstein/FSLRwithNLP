a7f0cb6ecbfa0750567ae8a88820a85a241f9b41
Lord Dickenstein <jddestacamento@iskolarngbayan.pup.edu.ph>
2021-09-18 17:58:45 +0800

    Removed duplcated dataset in FingerSpelling Folder

---
+++ Tutorials/CNN_TRY.py
@@ -0,0 +1,12 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import os  # iterate through the directories
+import cv2
+
+data_dir = "D:/Documents/Thesis/FSLRwithNLP/Datasets/Fingerspelling/sign_mnist_train.csv"
+categories = ['A', 'B', 'C', 'D', 'E', 'F',
+              'G', 'H', 'I', 'K', 'L', 'M',
+              'N', 'O', 'P', 'Q', 'R', 'S',
+              'T', 'U', 'V', 'W', 'X', 'Y']
+
+

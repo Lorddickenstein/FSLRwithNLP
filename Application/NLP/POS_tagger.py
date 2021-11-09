625eefd2cc383d4e75a4450fbb9fb22080f857b2
ssamushua <joshuacrzt@gmail.com>
2021-11-09 15:59:21 +0800

    ll

--- Application/NLP/POS_tagger.py
+++ Application/NLP/POS_tagger.py
@@ -1,10 +1,6 @@
 import nltk
-<<<<<<< HEAD:Application/POS_tagger.py
 from nltk.tag import pos_tag
-=======
 nltk.download('averaged_perceptron_tagger')
->>>>>>> f9ad38df7212fbcb0acacc3caaac68d668dae15f:Application/NLP/POS_tagger.py
-from easynmt import EasyNMT
 
 def tokenizer(text):
   return nltk.word_tokenize(text)

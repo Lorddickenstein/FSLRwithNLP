41495c71a13ed246a43401c3860793a75c2aabb0
Lord Dickenstein <jddestacamento@iskolarngbayan.pup.edu.ph>
2021-11-09 16:12:35 +0800

    something

--- Application/NLP/POS_tagger.py
+++ Application/NLP/POS_tagger.py
@@ -1,6 +1,7 @@
 import nltk
-nltk.download('averaged_perceptron_tagger')
+from Application.NLP.Repos import read_file
 from easynmt import EasyNMT
+nltk.download('averaged_perceptron_tagger')
 
 def tokenizer(text):
   return nltk.word_tokenize(text)
@@ -13,8 +14,15 @@ def main():
 
   text = "Good Morning"
   tokens = tokenizer(text)
+  # print(tokens)
   pos_tagged = pos_tag(tokens)
 
   print("POS TAGGED: ", pos_tagged)
 
-main()
+# main()
+
+def test():
+  dictionary = read_file('D:\Documents\Thesis\FSLRwithNLP\Application\\NLP\dictionary.txt')
+  print(pos_tag(dictionary))
+
+test()

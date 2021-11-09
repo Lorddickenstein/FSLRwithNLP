f4ac50c79b1884476aa10d83c1d77e7a9b30b1d1
Rocella Mae Legaspi <ellelgsp@gmail.com>
2021-11-09 21:47:49 +0800

    new Gui

--- workingFormat.py
+++ workingFormat.py
@@ -4,22 +4,12 @@ import cv2
 from PIL import Image, ImageTk
 from datetime import datetime
 import os
-# import tensorflow as tf
-# from tensorflow import keras
-# import numpy as np
-# import mediapipe as mp
-# import time
-# import Application.utils as utils
-# import Application.HandTrackingModule as HTM
-# import Application.SignClassificationModule as SCM
-# import matplotlib.pyplot as plt
-# import warnings
 import imutils
 
 
 def showFeed():
     ret, frame = cap.read()
-    frame = imutils.resize(frame, width=1000)
+    frame = imutils.resize(frame, width=815)
     height, width, channel = frame.shape
     # print(height, width)
 
@@ -64,55 +54,12 @@ def endCapture():
         window.is_capturing = False
 
 
-# def stopCam():
-#     cap.release()
-#     camBut.config(text="START CAMERA", command=startCam)
-#     camLabel.config(text="OFF CAM", font=("Montserrat", 12))
-#
-#
-# def startCam():
-#     cap = cv2.VideoCapture(0)
-#     width_1, height_1 = 1080, 720
-#     cap.set(cv2.CAP_PROP_FRAME_WIDTH, width_1)
-#     cap.set(cv2.CAP_PROP_FRAME_HEIGHT, height_1)
-#     camBut.config(text="STOP CAMERA", command=stopCam)
-#     camLabel.config(text="HERE", font="Montserrat")
-#     showFeed()
-
-# Radius of widget
-# def round_rectangle(x1, y1, x2, y2, radius=20, **kwargs):
-#     points = [x1 + radius, y1,
-#               x1 + radius, y1,
-#               x2 - radius, y1,
-#               x2 - radius, y1,
-#               x2, y1,
-#               x2, y1 + radius,
-#               x2, y1 + radius,
-#               x2, y2 - radius,
-#               x2, y2 - radius,
-#               x2, y2,
-#               x2 - radius, y2,
-#               x2 - radius, y2,
-#               x1 + radius, y2,
-#               x1 + radius, y2,
-#               x1, y2,
-#               x1, y2 - radius,
-#               x1, y2 - radius,
-#               x1, y1 + radius,
-#               x1, y1 + radius,
-#               x1, y1]
-#
-#     return leftFrame.create_polygon(points, **kwargs, smooth=True)
-# # startBut = round_rectangle(50, 50, 150, 100, radius=20, fill="blue")
-
 def homePage():
     window.destroy()
     import Home
 
 
 window = tk.Tk()
-# tf.get_logger().setLevel('ERROR')
-# warnings.simplefilter(action='ignore', category=Warning)
 
 window.is_capturing = False
 cap = cv2.VideoCapture(0)
@@ -122,51 +69,55 @@ cap.set(584, height)
 
 frm_arr = []
 
-window.geometry("1200x650+20+20")
+window.geometry("1300x680+20+20")
 window.resizable(False, False)
 window.title("FSLR Translator")
 window.configure(background="grey")
 
-leftFrame = tk.Canvas(window, width=700, height=584, bg="#c4c4c4")
-leftFrame.place(x=35, y=35)
+leftFrame = tk.Canvas(window, width=850, height=645, bg="#c4c4c4")
+leftFrame.place(x=15, y=15)
 
-rightFrame = tk.Canvas(window, width=400, height=584, bg="#6997F3")
-rightFrame.place(x=765, y=35)
+rightFrame = tk.Canvas(window, width=400, height=645, bg="#6997F3")
+rightFrame.place(x=880, y=15)
 
 camLabel = tk.Label(leftFrame, text="here", borderwidth=3, relief="groove")
-camLabel.place(x=30, y=30)
-startBut = tk.Button(leftFrame, width=20, height=2, text="START", bg="#1B7B03", font=("Montserrat", 9, "bold"),
+camLabel.place(x=20, y=20)
+
+startBut = tk.Button(rightFrame, width=25, height=2, text="START", bg="#1B7B03", font=("Montserrat", 9, "bold"),
                      command=startCapture)
-startBut.place(x=30, y=530)
-endBut = tk.Button(leftFrame, width=20, height=2, text="END", bg="#E21414", font=("Montserrat", 9, "bold"),
+startBut.place(x=15, y=15)
+justBut = tk.Button(rightFrame, width=25, height=2, bg="#c4c4c4", font=("Montserrat", 9, "bold"))
+justBut.place(x=15, y=60)
+endBut = tk.Button(rightFrame, width=25, height=2, text="END", bg="#E21414", font=("Montserrat", 9, "bold"),
                    command=endCapture)
-endBut.place(x=195, y=530)
-homeBut = tk.Button(leftFrame, width=20, height=2, text="HOME", bg="#2B449D", font=("Montserrat", 9, "bold"), command=homePage)
-homeBut.place(x=525, y=530)
+endBut.place(x=205, y=15)
+homeBut = tk.Button(rightFrame, width=25, height=2, text="HOME", bg="#2B449D", font=("Montserrat", 9, "bold"),
+                    command=homePage)
+homeBut.place(x=205, y=60)
 
-bowFrame = tk.Canvas(rightFrame, width=350, height=255, bg="#E84747")
-bowFrame.place(x=25, y=28)
-genLanFrame = tk.Canvas(rightFrame, width=350, height=255, bg="#E84747")
-genLanFrame.place(x=25, y=308)
+bowFrame = tk.Canvas(rightFrame, width=370, height=250, bg="#E84747")
+bowFrame.place(x=15, y=115)
+genLanFrame = tk.Canvas(rightFrame, width=370, height=250, bg="#E84747")
+genLanFrame.place(x=15, y=380)
 
-bowText = tk.Text(bowFrame, width=34, height=8, bg="#FDFAFA", font="Montserrat")
-bowText.place(x=23, y=48)
+bowText = tk.Text(bowFrame, width=38, height=8, bg="#FDFAFA", font="Montserrat")
+bowText.place(x=15, y=45)
 bowCountText = tk.Text(bowFrame, width=10, height=2, bg="#FDFAFA", font="Montserrat")
-bowCountText.place(x=236, y=208)
-genLanText = tk.Text(genLanFrame, width=34, height=8, bg="#FDFAFA", font="Montserrat")
-genLanText.place(x=23, y=48)
+bowCountText.place(x=267, y=200)
+genLanText = tk.Text(genLanFrame, width=38, height=8, bg="#FDFAFA", font="Montserrat")
+genLanText.place(x=15, y=45)
 genLanCountText = tk.Text(genLanFrame, width=10, height=2, bg="#FDFAFA", font="Montserrat")
-genLanCountText.place(x=236, y=208)
+genLanCountText.place(x=267, y=200)
 
-bowLabel = tk.Label(bowFrame, text="BAG OF WORDS    :", bg="#E84747", fg="#FDFAFA", font=("Montserrat", 12, "bold"))
-bowLabel.place(x=23, y=16)
+bowLabel = tk.Label(bowFrame, text="BAG OF WORDS    :", bg="#E84747", fg="#FDFAFA", font=("Montserrat", 14, "bold"))
+bowLabel.place(x=15, y=10)
 bowCountLabel = tk.Label(bowFrame, text="COUNT    :", bg="#E84747", fg="#FDFAFA", font=("Montserrat", 12, "bold"))
-bowCountLabel.place(x=135, y=213)
+bowCountLabel.place(x=170, y=205)
 genLanLabel = tk.Label(genLanFrame, text="GENERATED LANGUAGE    :", bg="#E84747", fg="#FDFAFA",
-                       font=("Montserrat", 12, "bold"))
-genLanLabel.place(x=23, y=16)
+                       font=("Montserrat", 14, "bold"))
+genLanLabel.place(x=15, y=10)
 genLanCountLabel = tk.Label(genLanFrame, text="COUNT    :", bg="#E84747", fg="#FDFAFA", font=("Montserrat", 12, "bold"))
-genLanCountLabel.place(x=135, y=213)
+genLanCountLabel.place(x=170, y=205)
 
 showFeed()
 window.mainloop()
